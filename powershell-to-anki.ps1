d51b3ca9c289960453f0bc6c1f7e4f9b83e3f72c
Jacob Maiato <jmaiato1@outlook.com>
2024-10-06 21:19:32 -0400

    Create flashcard for command synopsis

--- powershell-to-anki.ps1
+++ powershell-to-anki.ps1
@@ -1,2 +1,94 @@
-(Get-Help Get-Help).Synopsis
-(Get-Help Get-Help).RelatedLinks.navigationLink[0].uri
+param(
+	[Parameter(Mandatory, ParameterSetName="SynopsisOnly")]
+	[Parameter(Mandatory, ParameterSetName="OptionOnly")]
+	[Parameter(Mandatory, ParameterSetName="SynopsisAndOption")]
+	[String]$Command ,
+
+	[Parameter(Mandatory, ParameterSetName="SynopsisOnly")]
+	[Parameter(ParameterSetName="OptionOnly")]
+	[Parameter(Mandatory, ParameterSetName="SynopsisAndOption")]
+	[Switch]$Synopsis ,
+
+	[Parameter(ParameterSetName="SynopsisOnly")]
+	[Parameter(Mandatory, ParameterSetName="OptionOnly")]
+	[Parameter(Mandatory, ParameterSetName="SynopsisAndOption")]
+	[String[]]$Parameter ,
+
+	[String]$AnkiConnectUri = "http://127.0.0.1:8765"
+)
+
+function Get-AnkiConnectRequestTable {
+	param(
+		[Parameter(Mandatory)]
+		[String]$Action ,
+
+		[Parameter(Mandatory)]
+		[Hashtable]$Parameter
+	)
+	return @{
+		action = $Action
+		params = $Parameter
+		version = 6
+	}
+}
+
+function Invoke-AnkiConnect {
+	param(
+		[Parameter(Mandatory)]
+		[String]$Action ,
+
+		[Parameter(Mandatory)]
+		[Hashtable]$Parameter
+	)
+	$RequestJson = ConvertTo-Json -Depth 4 (Get-AnkiConnectRequestTable $Action $Parameter)
+	$Response = Invoke-RestMethod -Uri $AnkiConnectUri -Body $RequestJson
+
+	if ($Response["error"])
+	{
+		throw $Response["error"]
+	}
+
+	return $Response.result
+}
+
+function New-AnkiNote {
+	param(
+		[Parameter(Mandatory)]
+		[String]$DeckName ,
+		[Parameter(Mandatory)]
+		[String]$Front ,
+		[Parameter(Mandatory)]
+		[String]$Back ,
+		[Parameter(Mandatory)]
+		[String]$Hint ,
+		[Parameter(Mandatory)]
+		[String]$Source ,
+		[Parameter(Mandatory)]
+		[String[]]$Tags
+	)
+	Invoke-AnkiConnect -Action "addNote" -Parameter @{
+		note = @{
+			deckName = $DeckName
+			modelName = "Basic"
+			fields = @{
+				Front = $Front
+				Back = $Back
+				Hint = $Hint
+				Source = $Source
+			}
+			tags = $Tags
+		}
+	}
+}
+
+$HelpPage = Get-Help $Command
+$OnlineHelpUri = $HelpPage.RelatedLinks.navigationLink[0].uri
+
+if ($Synopsis)
+{
+	$Front = $HelpPage.Synopsis
+	$Result = New-AnkiNote "Parent" $Front $Command "PowerShell command" $OnlineHelpUri @("PowerShell::Command")
+	Invoke-AnkiConnect -Action "guiBrowse" -Parameter @{
+		query = "nid:" + $Result
+	}
+}
