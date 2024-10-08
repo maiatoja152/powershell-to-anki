5dcf3530bd01aa2d6acdfadd96e536cf1d54ad32
jacob <jmaiato1@outlook.com>
2024-10-07 22:25:05 -0400

    Improve formatting on parameter cards and include parameter type

--- powershell-to-anki.ps1
+++ powershell-to-anki.ps1
@@ -80,22 +80,23 @@ function New-AnkiNote {
 	}
 }
 
-$HelpPage = Get-Help $Command
-$OnlineHelpUri = $HelpPage.RelatedLinks.navigationLink[0].uri
+$Help = Get-Help $Command
+$OnlineHelpUri = $Help.RelatedLinks.navigationLink[0].uri
 $NoteIds = @()
 
 if ($Synopsis) {
-	$Front = $HelpPage.Synopsis
-	$Example = '<div style="margin-top: 1rem">' + $HelpPage.examples.example[0].title + '</div>' + "<div>" + $HelpPage.examples.example[0].code + "</div>"
-	$Result = New-AnkiNote "Parent" $Front $Command "PowerShell command" $OnlineHelpUri @("PowerShell::Command") $Example
+	$Front = $Help.Synopsis
+	$Back = $Help.Name
+	$Example = "<div style=`"margin-top: 1rem;`">$($Help.examples.example[0].title)</div>`n<div>$($Help.examples.example[0].code)</div>"
+	$Result = New-AnkiNote "Parent" $Front $Back "PowerShell command" $OnlineHelpUri @("PowerShell::Command") $Example
 	$NoteIds += $Result
 }
 
 if ($Parameter.Count -gt 0) {
 	$Parameter | ForEach-Object {
-		$CurrentParameter = $HelpPage.parameters.parameter | Where-Object name -eq $PSItem
+		$CurrentParameter = $Help.parameters.parameter | Where-Object name -eq $PSItem
 		$Front = $CurrentParameter.description[0].Text
-		$Back = $CurrentParameter.name
+		$Back = "<b>-$($CurrentParameter.name)</b> &lt;$($CurrentParameter.type.name)&gt;"
 		$Result = New-AnkiNote "Parent" $Front $Back "PowerShell $Command parameter" $OnlineHelpUri @("PowerShell::Command::Parameter")
 		$NoteIds += $Result
 	}
